b25c1547e69854afe3f982ce307f936893a3f384
Javier Rojas <javierrojas6@gmail.com>
2022-03-05 15:26:20 -0500

    missing init files py

---
+++ PUJ/Model/__init__.py
@@ -0,0 +1,9 @@
+## =========================================================================
+## @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
+## =========================================================================
+
+from .Base import *
+from .Linear import *
+from .Logistic import *
+
+## eof - $RCSfile$
